49a565d780c02faba6fd63bad80daa622032aff3
Gabi Tessier <mtessier246@gmail.com>
2019-12-05 10:40:16 -0500

    CFG parsing is almost finished

--- src/SimpleExpressionParser.java
+++ src/SimpleExpressionParser.java
@@ -1,3 +1,6 @@
+import javax.swing.text.html.parser.Parser;
+import java.util.function.Function;
+
 /**
  * Starter code to implement an ExpressionParser. Your parser methods should use the following grammar:
  * E := A | X
@@ -34,4 +37,115 @@ public class SimpleExpressionParser implements ExpressionParser {
 		// TODO implement me
 		return null;
 	}
+
+	//im going to put as private but double check later
+
+	/**
+	 * Checks if the string follows the parsing rules for E
+	 * E -> A | X
+	 * @param str, the string being parsed
+	 * @return a boolean whether or not the the string follows the parsing rules.
+	 */
+	private boolean parseE(String str) {
+		if(parseA(str)) {
+			return true;
+		}
+		else if (parseX(str)) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Checks if the string follows the parsing rules for A
+	 * A -> A + M | M
+	 * @param str, the string being parsed
+	 * @return a boolean whether or not the the string follows the parsing rules.
+	 */
+	private boolean parseA(String str) {
+		if(parseHelper(str, '+', Parser::parseA, Parser::parseM)) {
+			return true;
+		}
+		else if (parseM(str)) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Checks if the string follows the parsing rules for M
+	 * M -> M * M | X
+	 * @param str, the string being parsed
+	 * @return a boolean whether or not the the string follows the parsing rules.
+	 */
+	private boolean parseM(String str) {
+		if(parseHelper(str, '*', Parser::parseM, Parser::parseM)) {
+			return true;
+		}
+		else if (parseX(str)) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Checks if the string follows the parsing rules for X
+	 * X -> (E) | L
+	 * @param str, the string being parsed
+	 * @return a boolean whether or not the the string follows the parsing rules.
+	 */
+	private boolean parseX(String str) {
+		for (int i = 1; i < str.length() - 1; i++) {
+			if (str.charAt(0, i) == '(' &&
+			     parseE(str.substring(i)) &&
+			     str.charAt(i+1) == ')') {
+				return true;
+			}
+		}
+		if (parseL(str)) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Checks if the string follows the parsing rules for L
+	 * L -> [a-z] | [0-9]
+	 * @param str, the string being parsed
+	 * @return a boolean whether or not the the string follows the parsing rules.
+	 */
+	private boolean parseL(String str) {
+		//TODO implement me
+		//checks if the string contains [a-z]
+		if (str.matches(".*[a-z].*")) {
+			return true;
+		}
+		//checks if the string contains [0-9]
+		else if (str.contains("[0-9]+")) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Abstracts the bounds for parsing mathmatical expressions
+	 * @param str, string that is being parsed
+	 * @param op, operation symbol (i.e. '+', '-', '*', '/')
+	 * @param m1, method 1
+	 * @param m2, method 2
+	 * @return a boolean whether or not the the string follows the parsing rules.
+	 */
+	private boolean parseHelper(String str, char op,
+								Function<String, Boolean> m1,
+								Function<String, Boolean> m2) {
+		for(int i = 1; i < str.length() -1; i++) {
+			if (str.charAt(i) == op &&
+			     m1(str.substring(0, i)) &&
+			     m2(str.substring(i+1))) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
