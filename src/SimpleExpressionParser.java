9f6f64623d2a783041d5ab03d9d351eabcd38375
Ted Clifford <edwardfclifford@gmail.com>
2019-12-06 21:30:24 -0500

    Implemented add to tree as you go strategy

--- src/SimpleExpressionParser.java
+++ src/SimpleExpressionParser.java
@@ -9,135 +9,161 @@ import java.util.function.Function;
  * L := [0-9]+ | [a-z]
  */
 public class SimpleExpressionParser implements ExpressionParser {
-	/*
-	 * Attempts to create an expression tree -- flattened as much as possible -- from the specified String.
-	 * Throws a ExpressionParseException if the specified string cannot be parsed.
-	 * @param str the string to parse into an expression tree
-	 * @param withJavaFXControls you can just ignore this variable for R1
-	 * @return the Expression object representing the parsed expression tree
-	 */
-	public Expression parse (String str, boolean withJavaFXControls) throws ExpressionParseException {
-		// Remove spaces -- this simplifies the parsing logic
-		str = str.replaceAll(" ", "");
-		Expression expression = parseExpression(str);
-		if (expression == null) {
-			// If we couldn't parse the string, then raise an error
-			throw new ExpressionParseException("Cannot parse expression: " + str);
-		}
+    /*
+     * Attempts to create an expression tree -- flattened as much as possible -- from the specified String.
+     * Throws a ExpressionParseException if the specified string cannot be parsed.
+     * @param str the string to parse into an expression tree
+     * @param withJavaFXControls you can just ignore this variable for R1
+     * @return the Expression object representing the parsed expression tree
+     */
+    public Expression parse (String str, boolean withJavaFXControls) throws ExpressionParseException {
+        // Remove spaces -- this simplifies the parsing logic
+        str = str.replaceAll(" ", "");
+        Expression expression = parseExpression(str);
+        if (expression == null) {
+            // If we couldn't parse the string, then raise an error
+            throw new ExpressionParseException("Cannot parse expression: " + str);
+        }
 
-		// Flatten the expression before returning
-		expression.flatten();
-		return expression;
-	}
+        // Flatten the expression before returning
+        expression.flatten();
+        return expression;
+    }
 
-	protected Expression parseExpression (String str) {
-		Expression expression;
-		// TODO implement me
-		//createTree(str);
-		return null;
-	}
+    protected Expression parseExpression (String str) {
+        Expression expression;
+        // TODO implement me
+        //createTree(str);
+        return null;
+    }
 
-	//im going to put as private but double check later
-	/**
-	 * Checks if the string follows the parsing rules for A
-	 * E -> E + M | M
-	 * @param str, the string being parsed
-	 * @return a boolean whether or not the the string follows the parsing rules.
-	 */
-	private static Expression parseE(String str) {
-		Expression express_parser = parseHelper(str, '+', SimpleExpressionParser::parseE, SimpleExpressionParser::parseM);
-		if(express_parser != null) {
-			//adds the operation, '+'
+    //im going to put as private but double check later
+    /**
+     * Checks if the string follows the parsing rules for A
+     * E -> E + M | M
+     * @param str, the string being parsed
+     * @param parent the expression the parsed string will belong to
+     * @return the valid expression, or null if none exists 
+     */
+    private static Expression parseE(String str, CompoundExpression parent) {
 
-			return express_parser;
-		}
-		else if (parseM(str) != null) {
-			//TODO CHECK THIS
-			return parseM(str);
-		}
-		return null;
-	}
+        // Checking E+M
+        final CompoundExpression addExpression = new AddExpression();
+        if (parseHelper(str, '+', addExpression, SimpleExpressionParser::parseE, SimpleExpressionParser::parseM) != null) {
+            parent.addSubexpression(addExpression);
+            return addExpression;
+        }
 
-	/**
-	 * Checks if the string follows the parsing rules for M
-	 * M -> M * X | X
-	 * @param str, the string being parsed
-	 * @return a boolean whether or not the the string follows the parsing rules.
-	 */
-	private static Expression parseM(String str) {
-		Expression value = parseHelper(str, '*', SimpleExpressionParser::parseM, SimpleExpressionParser::parseX);
-		if(value != null) {
-			return value;
-		}
-		else if (parseX(str) != null) {
-			return null;
-		}
-		return null;
-	}
+        // Checking M
+        final CompoundExpression multExpression = new MultiplicativeExpression();
+        if (parseM(str, multExpression) != null) {
+            //TODO CHECK THIS
+            parent.addSubexpression(multExpression);
+            return multExpression;
+        }
+        return null;
+    }
 
-	/**
-	 * Checks if the string follows the parsing rules for X
-	 * X -> (E) | L
-	 * @param str, the string being parsed
-	 * @return a boolean whether or not the the string follows the parsing rules.
-	 */
-	private static Expression parseX(String str) {
-		for (int i = 1; i < str.length() - 1; i++) {
-			if (str.charAt(0) == '(' &&
-					(parseE(str.substring(i-1, i)) != null) &&
-					str.charAt(i+1) == ')') {
-				//add
-				//wwhhhatat expression do i returnnnnnnn
-				return null;
-			}
-		}
-		if (parseL(str) != null) {
-			return null;
-		}
-		return null;
-	}
+    /**
+     * Checks if the string follows the parsing rules for M
+     * M -> M * X | X
+     * @param str, the string being parsed
+     * @param parent the expression the parsed string will belong to
+     * @return the valid expression, or null if none exists 
+     */
+    private static Expression parseM(String str, CompoundExpression parent) {
 
-	/**
-	 * Checks if the string follows the parsing rules for L
-	 * L -> [a-z] | [0-9]
-	 * @param str, the string being parsed
-	 * @return a boolean whether or not the the string follows the parsing rules.
-	 */
-	private static Expression parseL(String str) {
-		//checks if the string contains [a-z]
-		if (str.matches(".*[a-z].*")) {
-			return null;
-		}
-		//checks if the string contains [0-9]
-		else if (str.contains("[0-9]+")) {
-			return null;
-		}
-		return null;
-	}
+        // Checking M*X
+        final CompoundExpression multExpression = new MultExpression();
+        if(parseHelper(str, '*', multExpression, SimpleExpressionParser::parseM, SimpleExpressionParser::parseX);!= null) {
+            parent.addSubExpression(multExpression);
+            return multExpression;
+        }
 
-	/**
-	 * Abstracts the bounds for parsing mathematical expressions
-	 * @param str, string that is being parsed
-	 * @param op, operation symbol (i.e. '+' or '*')
-	 * @param m1, method 1
-	 * @param m2, method 2
-	 * @return a boolean whether or not the the string follows the parsing rules.
-	 */
-	public static Expression parseHelper(String str, char op,
-								  Function<String, Expression> m1,
-								  Function<String, Expression> m2) {
-		//TODO finish this method's return objects
-		CompoundExpressionImpl CEI = new CompoundExpressionImpl();
-		for(int i = 1; i < str.length() -1; i++) {
-			final Expression method1 = m1.apply(str.substring(0, i));
-			final Expression method2 = m2.apply(str.substring(i + 1));
-			if ((str.charAt(i) == op) &&
-					(method1 != null) &&
-					(method2 != null)) {
-				//means both method 1 and two are NOT literals
-				return null;
-			}
-		}
-		return null;
-	}
+        // Checking X
+        final CompoundExpression parenExpression = new ParentheticalExpression();
+        if (parseX(str, parenExpression) != null) {
+            parent.addSubexpression(parenExpression);
+            return parenExpression;
+        }
+
+        return null;
+    }
+
+    /**
+     * Checks if the string follows the parsing rules for X
+     * X -> (E) | L
+     * @param str, the string being parsed
+     * @param parent the expression the parsed string will belong to
+     * @return the valid expression, or null if none exists 
+     */
+    private static Expression parseX(String str, CompoundExpression parent) {
+
+        // Checking (E)
+        for (int i = 1; i < str.length() - 1; i++) {
+            final CompoundExpression parenExpression = new ParentheticalExpression();
+
+            if (str.charAt(0) == '(' &&
+                    (parseE(str.substring(i-1, i), parenExpression) != null) &&
+                    str.charAt(i+1) == ')') {
+                parent.addSubexpression(parenExpression);
+                return parenExpression;
+            }
+        }
+        
+        // Checking L
+        if (parseL(str, parent) != null) {
+            return new LiteralExpression(str);
+        }
+
+        return null;
+    }
+
+    /**
+     * Checks if the string follows the parsing rules for L
+     * L -> [a-z] | [0-9]
+     * @param str, the string being parsed
+     * @param parent the expression the parsed string will belong to
+     * @return the valid expression, or null if none exists 
+     */
+    private static Expression parseL(String str, CompoundExpression parent) {
+    
+        //checks if the string contains [a-z] or is a number
+        if (str.matches(".*[a-z].*") || str.contains("[0-9]+")) {
+            final Expression literal = new LiteralExpression(str);
+            parent.addSubexpression(literal);
+            return literal;
+        }
+
+        return null;
+    }
+
+    /**
+     * Abstracts the bounds for parsing mathematical expressions
+     * @param str, string that is being parsed
+     * @param op, operation symbol (i.e. '+' or '*')
+     * @param parent the expression the parsed string will belong to
+     * @param m1, method 1
+     * @param m2, method 2
+     * @return the valid expression, or null if none exists 
+     */
+    public static Expression parseHelper(String str, char op, CompoundExpression parent,
+                                  Function<String, Expression> m1,
+                                  Function<String, Expression> m2) {
+
+        for(int i = 1; i < str.length() -1; i++) {
+            final Expression leftExpression = m1.apply(str.substring(0, i), parent);
+            final Expression rightExpression = m2.apply(str.substring(i + 1), parent);
+            if ((str.charAt(i) == op) &&
+                    (leftExpression != null) &&
+                    (rightExpression != null)) {
+
+                parent.addSubexpression(leftExpression);
+                parent.addSubexpression(rightExpression);
+
+                return parent;
+            }
+        }
+        return null;
+    }
 }
