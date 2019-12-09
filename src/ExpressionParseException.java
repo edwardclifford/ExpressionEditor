fb451eb7e81d473059e487fa15e99a7fdf5e26d1
Edward Clifford <edward.clifford@monomoy.edu>
2019-12-09 14:00:03 -0500

    Added R2 Basecode

--- src/ExpressionParseException.java
+++ src/ExpressionParseException.java
@@ -1,8 +1,5 @@
-/**
- * Exception thrown when a ExpressionParser fails to parse a specified string.
- */
 class ExpressionParseException extends Exception {
-    public ExpressionParseException (String message) {
-        super(message);
-    }
+	public ExpressionParseException (String message) {
+		super(message);
+	}
 }
